89e214a008d418fdf7db42996f056cf7f0d19e43
Kirill Kamratov <kirill.kamratov@yandex.ru>
2022-10-08 01:55:51 +0300

    feat(nav-item): remade to functional

--- src/components/nav-item/nav-item.jsx
+++ src/components/nav-item/nav-item.jsx
@@ -2,29 +2,21 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import navItemStyles from './nav-item.module.css'
 
-class NavItem extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const Icon = this.props.icon
-    const textColorType =
-      this.props.type === 'primary'
-        ? 'text_color_primary'
-        : 'text_color_inactive'
-    return (
-      <a
-        className={`pt-4 pr-5 pb-4 pl-5 ${navItemStyles.item}`}
-        href='#'
-      >
-        <Icon type={this.props.type} />
-        <p className={`text text_type_main-default ${textColorType} ml-2`}>
-          {this.props.children}
-        </p>
-      </a>
-    )
-  }
+function NavItem({ icon, type, children }) {
+  const Icon = icon
+  const textColorType =
+    type === 'primary' ? 'text_color_primary' : 'text_color_inactive'
+  return (
+    <a
+      className={`pt-4 pr-5 pb-4 pl-5 ${navItemStyles.item}`}
+      href='#'
+    >
+      <Icon type={type} />
+      <p className={`text text_type_main-default ${textColorType} ml-2`}>
+        {children}
+      </p>
+    </a>
+  )
 }
 
 NavItem.propTypes = {
