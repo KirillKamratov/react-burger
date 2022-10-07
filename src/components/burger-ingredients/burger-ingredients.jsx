97e0b5887f35d376cb70c9e5c7727067309cc26d
Kirill Kamratov <kirill.kamratov@yandex.ru>
2022-10-08 02:06:58 +0300

    feat(burgerIngredients): remade to functional

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -3,97 +3,86 @@ import burgerIngredientsStyles from './burger-ingredients.module.css'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 import IngredientsGrid from '../ingredients-grid'
 
-class BurgerIngredients extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: 'Булки',
-    }
+function BurgerIngredients() {
+  const [state, setState] = React.useState('Булки')
+
+  const handleValueSwitch = value => {
+    setState(value)
   }
 
-  handleValueSwitch = value => {
-    this.setState({
-      value: value,
-    })
-  }
-
-  render() {
-    return (
-      <>
-        <h1
-          className={`mt-10 mb-5 text text_color_primary text_type_main-large`}
-        >
-          Соберите Бургер
-        </h1>
-        <nav className={`mb-10`}>
-          <ul className={`${burgerIngredientsStyles.menu}`}>
-            <li>
-              <a
-                className={burgerIngredientsStyles.button}
-                href='#bun'
+  return (
+    <>
+      <h1 className={`mt-10 mb-5 text text_color_primary text_type_main-large`}>
+        Соберите Бургер
+      </h1>
+      <nav className={`mb-10`}>
+        <ul className={`${burgerIngredientsStyles.menu}`}>
+          <li>
+            <a
+              className={burgerIngredientsStyles.button}
+              href='#bun'
+            >
+              <Tab
+                active={state.value === 'Булки'}
+                onClick={handleValueSwitch}
+                value={'Булки'}
               >
-                <Tab
-                  active={this.state.value === 'Булки'}
-                  onClick={this.handleValueSwitch}
-                  value={'Булки'}
-                >
-                  Булки
-                </Tab>
-              </a>
-            </li>
-            <li>
-              <a
-                className={burgerIngredientsStyles.button}
-                href='#sauce'
-              >
-                <Tab
-                  active={this.state.value === 'Соусы'}
-                  onClick={this.handleValueSwitch}
-                  value={'Соусы'}
-                >
-                  Соусы
-                </Tab>
-              </a>
-            </li>
-            <li>
-              <a
-                className={burgerIngredientsStyles.button}
-                href='#main'
-              >
-                <Tab
-                  active={this.state.value === 'Начинки'}
-                  onClick={this.handleValueSwitch}
-                  value={'Начинки'}
-                >
-                  Начинки
-                </Tab>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <ul className={`${burgerIngredientsStyles.list}`}>
-          <li className={burgerIngredientsStyles.item}>
-            <IngredientsGrid
-              type='bun'
-              text='Булки'
-            />
+                Булки
+              </Tab>
+            </a>
           </li>
-          <li className={burgerIngredientsStyles.item}>
-            <IngredientsGrid
-              type='sauce'
-              text='Соусы'
-            />
+          <li>
+            <a
+              className={burgerIngredientsStyles.button}
+              href='#sauce'
+            >
+              <Tab
+                active={state.value === 'Соусы'}
+                onClick={handleValueSwitch}
+                value={'Соусы'}
+              >
+                Соусы
+              </Tab>
+            </a>
           </li>
-          <li className={burgerIngredientsStyles.item}>
-            <IngredientsGrid
-              type='main'
-              text='Начинки'
-            />
+          <li>
+            <a
+              className={burgerIngredientsStyles.button}
+              href='#main'
+            >
+              <Tab
+                active={state.value === 'Начинки'}
+                onClick={handleValueSwitch}
+                value={'Начинки'}
+              >
+                Начинки
+              </Tab>
+            </a>
           </li>
         </ul>
-      </>
-    )
-  }
+      </nav>
+      <ul className={`${burgerIngredientsStyles.list}`}>
+        <li className={burgerIngredientsStyles.item}>
+          <IngredientsGrid
+            type='bun'
+            text='Булки'
+          />
+        </li>
+        <li className={burgerIngredientsStyles.item}>
+          <IngredientsGrid
+            type='sauce'
+            text='Соусы'
+          />
+        </li>
+        <li className={burgerIngredientsStyles.item}>
+          <IngredientsGrid
+            type='main'
+            text='Начинки'
+          />
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default BurgerIngredients
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,23 +3,17 @@ import mainStyles from './main.module.css'
 import BurgerIngredients from '../burger-ingredients'
 import BurgerConstructor from '../burgrer-constructor'
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <main className={mainStyles.main}>
-        <section className={`${mainStyles.constructor}`}>
-          <BurgerIngredients />
-        </section>
-        <section className={`pt-25`}>
-          <BurgerConstructor />
-        </section>
-      </main>
-    )
-  }
+function Main() {
+  return (
+    <main className={mainStyles.main}>
+      <section className={`${mainStyles.constructor}`}>
+        <BurgerIngredients />
+      </section>
+      <section className={`pt-25`}>
+        <BurgerConstructor />
+      </section>
+    </main>
+  )
 }
 
 export default Main
