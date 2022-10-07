59bd23586bd8871b943c3bf3255704489e96dc6d
Kirill Kamratov <kirill.kamratov@yandex.ru>
2022-10-08 01:58:02 +0300

    feat(header): remade to functional

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,46 +8,40 @@ import {
   ProfileIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components'
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <header className={headerStyles.header}>
-        <nav className={headerStyles.navigation}>
-          <ul className={headerStyles.list}>
-            <li className={headerStyles.item}>
-              <NavItem
-                type={'primary'}
-                icon={BurgerIcon}
-              >
-                Конструктор
-              </NavItem>
-              <NavItem
-                type={'secondary'}
-                icon={ListIcon}
-              >
-                Лента заказов
-              </NavItem>
-            </li>
-            <li className={headerStyles.item}>
-              <Logo />
-            </li>
-            <li className={headerStyles.item}>
-              <NavItem
-                type={'secondary'}
-                icon={ProfileIcon}
-              >
-                Личный кабинет
-              </NavItem>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
-  }
+function Header() {
+  return (
+    <header className={headerStyles.header}>
+      <nav className={headerStyles.navigation}>
+        <ul className={headerStyles.list}>
+          <li className={headerStyles.item}>
+            <NavItem
+              type={'primary'}
+              icon={BurgerIcon}
+            >
+              Конструктор
+            </NavItem>
+            <NavItem
+              type={'secondary'}
+              icon={ListIcon}
+            >
+              Лента заказов
+            </NavItem>
+          </li>
+          <li className={headerStyles.item}>
+            <Logo />
+          </li>
+          <li className={headerStyles.item}>
+            <NavItem
+              type={'secondary'}
+              icon={ProfileIcon}
+            >
+              Личный кабинет
+            </NavItem>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
